48a6ef075e2e253695aa307ae7d4da72cfc0d31a
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-04-24 15:10:41 +0300

    added url class

---
+++ Server/Data/URL.cs
@@ -0,0 +1,13 @@
+namespace BlazorJWT_Auth.Shared
+{
+    public class URL
+    {
+        //URL.Auth.Register
+        public class Auth
+        {
+            public string Register = "123";
+            public string Login = "123";
+            public string RefreshToken = "123";
+        }
+    }
+}
